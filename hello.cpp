85a38b87148b5e8cad47df3f629e5fffb82fdaf6
arshanrodrigues <83663176+arshanrodrigues@users.noreply.github.com>
2023-02-17 21:09:59 +0530

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "My name is Arshan my SRN is PES2UG20CS067\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
