c59e8a318a08de3864f27ca1a4d81d117938cbf6
arshanrodrigues <arshanevra3@gmail.com>
2023-02-17 20:46:20 +0530

    Modified hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "My name is Arshan and my SRN is PES2UG20CS067\n";
+  cout << "My name is Arshan my SRN is PES2UG20CS067\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
   return 0;
